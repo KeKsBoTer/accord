e0a7a9b2725e7a0a74bb790c2db14960bde0e8ed
Luca Manzi <ronin@icecream.localdomain>
2021-09-25 14:53:35 +0200

    ips and ports 2.1

--- get_ip.py
+++ get_ip.py
@@ -5,7 +5,7 @@ import time
 
 portspace = (49152, 65535)
 ##
-randomfixedport = random.randint(portspace)
+randomfixedport = random.randint(*portspace)
 ##
 max_node_number = 64
 
@@ -20,9 +20,8 @@ stdout,stderr = MyOut.communicate()
 # print(stdout)
 # print(stderr)
 
-print(type(stdout))
 arr = str(stdout, 'utf-8').split('\n')[:-1]#last one is
-print(len(arr))
+
 for item in arr:
     print(socket.gethostbyname(item),":",randomfixedport)
 
