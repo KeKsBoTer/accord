0fbad0fe0a33c5cffdad26684ecd894395d87410
Luca Manzi <ronin@icecream.localdomain>
2021-09-25 20:04:09 +0200

    testing...

--- get_ip.py
+++ get_ip.py
@@ -36,14 +36,16 @@ for i in range(0,(len(available_nodes[1:5])-1)):
         f"ssh", 
         f"-f",
         f"{i}",
-        "\""
+        "\"",
             "./target/debug/accord",
             f"{available_nodes[i+1]}:{entry_node_p}"
             f"{available_nodes}:{entry_node_p}",
             f"{available_nodes}:{ws_port}",
-            "--entry-node", f"{available_nodes[0]}:{entry_node_p}"]])
-            # available_nodes[i] would do the trick.
+            "--entry-node", f"{available_nodes[0]}:{entry_node_p}"]],
         "\""
+        )
+            # available_nodes[i] would do the trick.
+
     processes.append(p)
 for p in processes:
     p.wait()
